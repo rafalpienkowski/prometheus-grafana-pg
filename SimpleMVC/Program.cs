8a4f10995e3ad0032c4f077c893271cf7a397986
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2019-03-17 17:10:21 +0100

    first commit

---
+++ SimpleMVC/Program.cs
@@ -0,0 +1,23 @@
+﻿using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SimpleMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseMetricsWebTracking()
+                .UseMetricsEndpoints()
+                //.UseHealth()
+                .UseHealthEndpoints()
+                .UseMetrics()
+                .UseStartup<Startup>();
+    }
+}
